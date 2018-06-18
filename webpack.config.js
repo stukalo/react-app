22bcec090a7ddbe06985726252e17eb1f8ddbc48
Stukalo Oleksandr <stukalo.oleksandr@gmail.com>
2018-06-18 18:26:46 +0300

    initialize

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  context: path.resolve(__dirname, 'src'),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public/assets'),
+  }
+};
